61061ef16f0f8f645429c2054e6a5b7679c90d51
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-27 05:39:05 -0800

    cache token

--- src/HttpClientAuthorizationSample/AuthorizedHttpMessageHandler.cs
+++ src/HttpClientAuthorizationSample/AuthorizedHttpMessageHandler.cs
@@ -9,13 +9,16 @@ namespace HttpClientAuthorizationSample;
 
 public sealed class AuthorizedHttpMessageHandler(HttpClient tokenHttpClient) : DelegatingHandler
 {
+  private const int CacheGapInMilliseconds = 500;
+
   private readonly HttpClient _tokenHttpClient = tokenHttpClient ?? throw new ArgumentNullException(nameof(tokenHttpClient));
+
+  private Token? _cachedToken;
+  private DateTimeOffset _cacheExpiresAt;
 
   protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
   {
-    Token token = await _tokenHttpClient.GetFromJsonAsync<Token>("", cancellationToken) ??
-                  throw new Exception("No token retrieved");
-
+    Token token = await RetrieveToken(cancellationToken);
     request.Headers.Authorization = new AuthenticationHeaderValue
     (
       scheme: token.TokenType,
@@ -23,5 +26,19 @@ public sealed class AuthorizedHttpMessageHandler(HttpClient tokenHttpClient) : D
     );
 
     return await base.SendAsync(request, cancellationToken);
+  }
+
+  private async Task<Token> RetrieveToken(CancellationToken cancellationToken)
+  {
+    if (_cachedToken is null || _cacheExpiresAt < DateTimeOffset.UtcNow.AddMilliseconds(CacheGapInMilliseconds))
+    {
+      Token token = await _tokenHttpClient.GetFromJsonAsync<Token>("token", cancellationToken) ??
+                    throw new Exception("No token retrieved");
+
+      _cachedToken = token;
+      _cacheExpiresAt = DateTimeOffset.UtcNow.AddSeconds(token.ExpiresIn);
+    }
+
+    return _cachedToken;
   }
 }
