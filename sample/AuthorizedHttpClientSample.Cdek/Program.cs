9a51f221f11ceb8540e6e075693477a7dea98d1c
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-30 10:38:12 -0800

    add cdek console sample

--- sample/AuthorizedHttpClientSample.Cdek/Program.cs
+++ sample/AuthorizedHttpClientSample.Cdek/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AuthorizedHttpClientSample;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+ConfigurationBuilder builder = new();
+builder.AddInMemoryCollection(new Dictionary<string, string?>
+{
+  { "TokenBaseUrl", "" },
+  { "GetTokenUrl" , "" },
+  { "ClientId"    , "" },
+  { "ClientSecret", "" },
+  { "ApiBaseUrl"  , "" },
+});
+
+IConfiguration configuration = builder.Build();
+
+ServiceCollection services = new();
+services.AddSingleton(configuration);
+services.AddAuthorizedHttpClient();
+
+using IServiceScope scope = services.BuildServiceProvider().CreateScope();
+
+HttpClient http = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>().CreateClient("api");
